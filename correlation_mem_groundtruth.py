17cffa055767679ae20d8406fa2e76dd25f515b2
Josik SALLAUD <josik.sallaud@etu.univ-nantes.fr>
2022-12-13 15:43:21 +0100

    Corrélation mémorabilité et vérité-terrain TVSum

---
+++ correlation_mem_groundtruth.py
@@ -0,0 +1,79 @@
+import cv2
+import torch
+from read_tvsum import get_frames_importance
+from resmem import ResMem, transformer
+from PIL import Image
+import matplotlib.pyplot as plt
+from scipy import stats
+
+VIDEO_FOLDER = './tvsum/video/'
+VIDEO_ID = 'EE-bNr36nyA'
+VIDEO_NAME = VIDEO_ID + '.mp4'
+
+DISPLAY_VIDEO = True
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(VIDEO_FOLDER + VIDEO_NAME)
+   
+    model = ResMem(pretrained=True)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    memorability_scores = []
+
+    worst_image = None
+    best_image = None
+
+    i = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        i = i + 1
+
+        if not ret:
+            break
+
+        if i % 1 == 0:
+
+            # Display the resulting frame
+            image_x = transformer(Image.fromarray(frame))
+            image_x = image_x.to(device)
+            prediction = model(image_x.view(-1, 3, 227, 227))
+            
+            prediction = prediction.detach().cpu().numpy()[0][0]
+            memorability_scores.append(prediction)
+            if worst_image is None or prediction < worst_image[0]:
+                worst_image = (prediction, frame)
+
+            if best_image is None or prediction > best_image[0]:
+                best_image = (prediction, frame)
+
+            if DISPLAY_VIDEO:
+                cv2.imshow('Frame', frame)
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+    
+    
+    ground_truth = get_frames_importance(VIDEO_ID)
+    #memorability_scores_to_cor = np.array(memorability_scores).reshape(-1, 1)
+
+    print("Corrélation de Pearson : ", stats.pearsonr(memorability_scores, ground_truth))
+    print("Corrélation de Spearman : ", stats.spearmanr(memorability_scores, ground_truth))
+
+
+    # Affichage des pires et des meilleures image
+    cv2.imshow('Worst Memorability : ' + str(worst_image[0]), worst_image[1])
+    cv2.imshow('Best Memorability : ' + str(best_image[0]), best_image[1])
+
+    cv2.waitKey(10000)
+    # Affichage du graphique
+    plt.plot(memorability_scores, '-r')
+    plt.ylabel('Memorability of frames')
+    plt.xlabel(VIDEO_NAME)
+    plt.ylim([0.0, 1.0])
+    plt.xlim([0, len(memorability_scores)-1])
+    plt.show()
+
+

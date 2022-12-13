a2acf8cf3ccda2deb900f66f0792450655788330
Josik SALLAUD <josik.sallaud@etu.univ-nantes.fr>
2022-12-13 17:14:19 +0100

    Corrélation sur l'ensemble des vidéos

--- correlation_mem_groundtruth.py
+++ correlation_mem_groundtruth.py
@@ -5,75 +5,84 @@ from resmem import ResMem, transformer
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy import stats
+import glob
+import numpy as np
 
 VIDEO_FOLDER = './tvsum/video/'
-VIDEO_ID = 'EE-bNr36nyA'
-VIDEO_NAME = VIDEO_ID + '.mp4'
 
 DISPLAY_VIDEO = True
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(VIDEO_FOLDER + VIDEO_NAME)
-   
-    model = ResMem(pretrained=True)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    model.eval()
+    pearson_correlations = []
+    spearman_correlations = []
 
-    memorability_scores = []
+    video_names = glob.glob1(VIDEO_FOLDER, '*.mp4')
+    for video_name in video_names:
+        cap = cv2.VideoCapture(VIDEO_FOLDER + video_name)
+        model = ResMem(pretrained=True)
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+        model.eval()
 
-    worst_image = None
-    best_image = None
+        memorability_scores = []
 
-    i = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        i = i + 1
+        worst_image = None
+        best_image = None
+        
+        i = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            i = i + 1
+            if not ret:
+                break
 
-        if not ret:
-            break
+            if i % 1 == 0:
+                # Display the resulting frame
+                image_x = transformer(Image.fromarray(frame))
+                image_x = image_x.to(device)
+                prediction = model(image_x.view(-1, 3, 227, 227))
+                
+                prediction = prediction.detach().cpu().numpy()[0][0]
+                memorability_scores.append(prediction)
+                if worst_image is None or prediction < worst_image[0]:
+                    worst_image = (prediction, frame)
 
-        if i % 1 == 0:
+                if best_image is None or prediction > best_image[0]:
+                    best_image = (prediction, frame)
 
-            # Display the resulting frame
-            image_x = transformer(Image.fromarray(frame))
-            image_x = image_x.to(device)
-            prediction = model(image_x.view(-1, 3, 227, 227))
-            
-            prediction = prediction.detach().cpu().numpy()[0][0]
-            memorability_scores.append(prediction)
-            if worst_image is None or prediction < worst_image[0]:
-                worst_image = (prediction, frame)
+                if DISPLAY_VIDEO:
+                    cv2.imshow('Frame', frame)
+                    # Press Q on keyboard to  exit
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-            if best_image is None or prediction > best_image[0]:
-                best_image = (prediction, frame)
+        
+        video_id = video_name.split('.')[0]
+        ground_truth = get_frames_importance(video_id)
+        #memorability_scores_to_cor = np.array(memorability_scores).reshape(-1, 1)
+        pearson_correlation = stats.pearsonr(memorability_scores, ground_truth).statistic
+        pearson_correlations.append(pearson_correlation)
 
-            if DISPLAY_VIDEO:
-                cv2.imshow('Frame', frame)
-                # Press Q on keyboard to  exit
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+        spearman_correlation = stats.spearmanr(memorability_scores, ground_truth).correlation
+        spearman_correlations.append(spearman_correlation)
 
-    
-    
-    ground_truth = get_frames_importance(VIDEO_ID)
-    #memorability_scores_to_cor = np.array(memorability_scores).reshape(-1, 1)
+    print("--------------------------------")
+    print("Corrélation de Pearson : ")
+    print("- Moyenne : {} ".format(np.average(pearson_correlations)))
+    print("- Ecart-type : {} ".format(np.std(pearson_correlations)))
+    print("- Min : {} - Max : {}".format(np.min(pearson_correlations), np.max(pearson_correlations)))
+    print("--------------------------------")
+    print("Corrélation de Spearman : ")
+    print("- Moyenne : {} ".format(np.average(spearman_correlations)))
+    print("- Ecart-type : {} ".format(np.std(spearman_correlations)))
+    print("- Min : {} - Max : {}".format(np.min(spearman_correlations), np.max(spearman_correlations)))
+    print("--------------------------------")
 
-    print("Corrélation de Pearson : ", stats.pearsonr(memorability_scores, ground_truth))
-    print("Corrélation de Spearman : ", stats.spearmanr(memorability_scores, ground_truth))
-
-
-    # Affichage des pires et des meilleures image
-    cv2.imshow('Worst Memorability : ' + str(worst_image[0]), worst_image[1])
-    cv2.imshow('Best Memorability : ' + str(best_image[0]), best_image[1])
-
-    cv2.waitKey(10000)
     # Affichage du graphique
-    plt.plot(memorability_scores, '-r')
-    plt.ylabel('Memorability of frames')
-    plt.xlabel(VIDEO_NAME)
-    plt.ylim([0.0, 1.0])
-    plt.xlim([0, len(memorability_scores)-1])
+    plt.plot(spearman_correlations, '-r')
+    plt.plot(pearson_correlations, '-b', )
+    plt.ylabel('Corrélation')
+    plt.xlabel('Numéro des vidéos')
     plt.show()
 
 

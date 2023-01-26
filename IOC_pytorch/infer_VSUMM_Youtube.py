Fix normalisation + bins pour le plot

IOC_pytorch/infer_VSUMM_Youtube.py
@@ -37,6 +37,8 @@ if __name__ == "__main__":
     model.load_state_dict(torch.load(IOVC_WEIGHTS))
     model.to(device).eval()
    
+    normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
     with torch.no_grad():
 
         video_names = glob1(VIDEO_FOLDER, '*.flv') + glob1(VIDEO_FOLDER, '*.avi')
@@ -72,12 +74,7 @@ if __name__ == "__main__":
                         frame = preprocess(frame)
                         frame = frame.to(device)
 
-                        mean, std = frame.mean([1,2]), frame.std([1,2])
-
-                        if 0 in std:
-                            std[0] = std[1] = std[2] = 1
-
-                        frame = Normalize(mean, std)(frame).unsqueeze(0)
+                        frame = normalize(frame).unsqueeze(0)
 
                         past_frames.append(frame)
 

plot_distribution_iovc.py
@@ -3,16 +3,16 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 
-JSON_FILE = "./VSUMM-OpenVideo-iovc.json"
+JSON_FILE = "./SumMe-iovc.json"
 
 with open(JSON_FILE) as f:
     DB = json.load(f)
 
     for db_key in DB.keys():
-        db_data = np.around(np.asarray(DB[db_key]), decimals=3)
+        
+        # db_data = np.around(np.asarray(DB[db_key]), decimals=3)
+        db_data = np.asarray(DB[db_key])
 
-        w = Counter(db_data)
-
-        plt.bar(w.keys(), w.values())
+        plt.hist(db_data, bins=10)
         plt.title(db_key)
         plt.show()
9c01d8a28668a1082871e1f343f53a4b7e6d6ee7
Nathan Rocher <3007nat@gmail.com>
2023-02-02 16:50:13 +0100

    Ajout model

--- test_model_neural.py
+++ test_model_neural.py
@@ -9,7 +9,9 @@ import pickle
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.metrics import r2_score
 
+from rich.progress import Progress
 
 # Ground Truth
 groundtruth_videos = pd.read_csv('./TVSum-groundtruth.csv', sep=';', header=0).set_index('id')
@@ -27,7 +29,7 @@ big_df = None
 
 # Création du big tableau
 for key in iovc_videos.keys():
-   
+
     score_gt = np.array(groundtruth_videos.loc[key.replace(".mp4", ""), "importance"].split(",")).astype(float)
     score_iovc = np.array(iovc_videos[key].iloc[0], dtype=float)
     score_mem = np.array(memorability_videos.loc[key, "memorability_scores"].split(",")).astype(float)
@@ -50,30 +52,30 @@ for key in iovc_videos.keys():
     if big_df is None:
         big_df = df
     else:
-        big_df = pd.concat([big_df, df], axis= 0)
+        big_df = pd.concat([big_df, df], axis=0)
 
 print(big_df)
 
 ###################################################################################
 
 class MyDataset(torch.utils.data.Dataset):
- 
-  def __init__(self, data):
-    # scaler = StandardScaler()
-    # data_gt = data["gt"]
-    # data.drop(inplace=True, columns="gt")
-    # data = scaler.fit_transform(data)
-    self.data = data
 
-  def __len__(self):
-    return len(self.data)
-   
-  def __getitem__(self, idx):
-    b = self.data.iloc[idx]
-    x = b["gt"]
-    b = b.drop("gt")
-    b = b.to_numpy()
-    return torch.as_tensor(b), torch.as_tensor(x)
+    def __init__(self, data):
+        # scaler = StandardScaler()
+        # data_gt = data["gt"]
+        # data.drop(inplace=True, columns="gt")
+        # data = scaler.fit_transform(data)
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        b = self.data.iloc[idx]
+        x = b["gt"]
+        b = b.drop("gt")
+        b = b.to_numpy()
+        return torch.as_tensor(b), torch.as_tensor(x)
 
 class MLP(nn.Module):
     def __init__(self):
@@ -87,18 +89,20 @@ class MLP(nn.Module):
         )
 
     def forward(self, x):
-        x = self.layers(x)      
+        x = self.layers(x)
         return x
 
 ###################################################################################
 
 # Entrainement du réseau
 
+
 # Dataset
-train_df, test_df = train_test_split(big_df, test_size=0.25, random_state=1)
+train_df, test_df = train_test_split(big_df, test_size=0.1, random_state=1)
 train_df = MyDataset(train_df)
 test_df = MyDataset(test_df)
 trainloader = torch.utils.data.DataLoader(train_df, batch_size=256, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_df, batch_size=256, shuffle=True)
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -110,47 +114,80 @@ mlp.to(device)
 
 # Define the loss function and optimizer
 loss_function = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-5)
 
 # Run the training loop
-for epoch in range(0, 5): # 5 epochs at maximum
-
-    # Print epoch
-    print(f'Starting epoch {epoch+1}')
+for epoch in range(0, 5):  # 5 epochs at maximum
 
     # Set current loss value
     current_loss = 0.0
+    train_loss = 0.0
+    validation_loss = 0.0
+
+    s_o = torch.tensor([])
+    s_t = torch.tensor([])
+
+    s_o_v = torch.tensor([])
+    s_t_v = torch.tensor([])
 
     # Iterate over the DataLoader for training data
-    for i, data in enumerate(trainloader, 0):
+    with Progress() as progress:
+        task = progress.add_task("Epoch %d - Training" % (epoch), total=len(trainloader))
+        for i, data in enumerate(trainloader, 0):
 
-        # Get and prepare inputs
-        inputs, targets = data
-        inputs, targets = inputs.float().to(device), targets.float().to(device)
-        targets = targets.reshape((targets.shape[0], 1))
+            # Get and prepare inputs
+            inputs, targets = data
+            inputs, targets = inputs.float().to(device), targets.float().to(device)
+            targets = targets.reshape((targets.shape[0], 1))
 
-        # Zero the gradients
-        optimizer.zero_grad()
+            # Zero the gradients
+            optimizer.zero_grad()
 
-        # Perform forward pass
-        outputs = mlp(inputs)
+            # Perform forward pass
+            outputs = mlp(inputs)
 
-        # Compute loss
-        loss = loss_function(outputs, targets)
+            # Compute loss
+            loss = loss_function(outputs, targets)
 
-        # Perform backward pass
-        loss.backward()
+            # Perform backward pass
+            loss.backward()
 
-        # Perform optimization
-        optimizer.step()
+            # Perform optimization
+            optimizer.step()
 
-        # Print statistics
-        current_loss += loss.item()
-        if i % 10 == 0:
-            print('Loss after mini-batch %5d: %.3f' % (i + 1, current_loss / 500))
-            current_loss = 0.0
+            s_o = torch.cat((s_o, outputs.squeeze().cpu())).detach()
+            s_t = torch.cat((s_t, targets.squeeze().cpu())).detach()
+
+            # Print statistics
+            current_loss += loss.item()
+            train_loss += loss.item()
+            if i % 100 == 0:
+                progress.console.print('Loss after batch %5d: %.3f - R²=%.3f' % (i + 1, current_loss / 500, r2_score(s_t, s_o)))
+                current_loss = 0.0
+
+            progress.advance(task)
+
+    # Validation loop
+    with Progress() as progress:
+        task = progress.add_task("Epoch %d - Validation" % (epoch), total=len(testloader))
+        with torch.no_grad():
+
+            for y, test_data in enumerate(testloader, 0):
+                inputs, targets = test_data
+                inputs, targets = inputs.float().to(device), targets.float().to(device)
+                targets = targets.reshape((targets.shape[0], 1))
+                outputs = mlp(inputs)
+                loss = loss_function(outputs, targets)
+              
+                validation_loss += loss.item()
+
+                s_o_v = torch.cat((s_o_v, outputs.squeeze().cpu())).detach()
+                s_t_v = torch.cat((s_t_v, targets.squeeze().cpu())).detach()
+                
+                progress.advance(task)
+
+    print(f'Epoch {epoch} \t Training Loss={train_loss / len(trainloader)} - R²={r2_score(s_t, s_o)} \t Validation Loss={validation_loss / len(testloader)} - R²={r2_score(s_t_v, s_o_v)}\n')
+
 
 # Process is complete.
 print('Training process has finished.')
-
-
--- test_model_svr.py
+++ test_model_svr.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pickle
 
-
+TRAINING_MODEL = False
+TEST_VIDEOS = ["WxtbjNsCQ8A.mp4", "XzYM3PfTM4w.mp4", "kLxoNp-UchI.mp4", "eQu1rNs0an0.mp4", "VuWGsYPqAX8.mp4"]
 
 # Ground Truth
 groundtruth_videos = pd.read_csv('./TVSum-groundtruth.csv', sep=';', header=0).set_index('id')
@@ -21,7 +22,10 @@ emotion_videos = pd.read_json("./PROCESSED-TVsum-face-intensity.json", lines=Tru
 # Memorability
 memorability_videos = pd.read_csv('./TVSum-memorability.csv', sep=';', header=0).set_index("video_name")
 
-big_df = None
+# Dataframe 
+big_df = pd.DataFrame()
+test_df = pd.DataFrame()
+vid_df = pd.DataFrame()
 
 # Création du big tableau
 for key in iovc_videos.keys():
@@ -44,31 +48,67 @@ for key in iovc_videos.keys():
     df["iovc"] = score_iovc
     df["memorability"] = score_mem
     df["gt"] = score_gt
+    
+    # Création du jeux de test
+    if key in TEST_VIDEOS:
+        test_df = pd.concat([test_df, df], axis= 0)
 
-    if big_df is None:
-        big_df = df
+        # On récupère les données de la vidéo pour inférer les frames
+        if key == TEST_VIDEOS[0]:
+            vid_df = df
+
     else:
+        # Création du jeux train/validation
         big_df = pd.concat([big_df, df], axis= 0)
 
-print(big_df)
 
 
-# Entrainement du réseau
-train_df, test_df = train_test_split(big_df, test_size=0.25, random_state=1)
+# Split du jeu d'entrainement / validation
+train_df, valid_df = train_test_split(big_df, test_size=0.25, random_state=1)
 
+
+# Normalisation des données
 scaler = StandardScaler()
 
 train_df_gt = train_df["gt"]
 train_df.drop(inplace=True, columns="gt")
 train_df = scaler.fit_transform(train_df)
 
+valid_df_gt = valid_df["gt"]
+valid_df.drop(inplace=True, columns="gt")
+valid_df = scaler.transform(valid_df)
+
 test_df_gt = test_df["gt"]
 test_df.drop(inplace=True, columns="gt")
-test_df = scaler.fit_transform(test_df)
+test_df = scaler.transform(test_df)
 
+vid_df_gt = vid_df["gt"]
+vid_df.drop(inplace=True, columns="gt")
+vid_df = scaler.transform(vid_df)
+
+
+# Entrainement du réseau
 regr_model = svm.SVR()
-regr_model.fit(train_df, train_df_gt)
 
-print("Accuracy:", regr_model.score(test_df, test_df_gt))
+print("Using the model")
 
-pickle.dump(regr_model, open('model_regression_SVR_input_normalized.sav', 'wb'))
+if TRAINING_MODEL:
+    regr_model.fit(train_df, train_df_gt)
+    pickle.dump(regr_model, open('model_regression_SVR_input_normalized.sav', 'wb'))
+else:
+    regr_model = pickle.load(open('model_regression_SVR_input_normalized.sav', 'rb'))
+    print("Model loaded")
+
+# print("Accuracy R² sur jeux de validation :", regr_model.score(valid_df, valid_df_gt))
+# print("Accuracy R² sur jeux de test :", regr_model.score(test_df, test_df_gt))
+print(len(vid_df))
+# print("Accuracy R² sur la video à inférer :", regr_model.score(vid_df, vid_df_gt))
+
+vals_predicted = regr_model.predict(vid_df)
+print(vals_predicted)
+np.save("result_WxtbjNsCQ8A", vals_predicted)
+print(np.max(vals_predicted), np.min(vals_predicted), np.mean(vals_predicted))
+
+import matplotlib.pyplot as plt
+plt.plot(np.arange(0, len(vals_predicted)), vals_predicted)
+plt.show()

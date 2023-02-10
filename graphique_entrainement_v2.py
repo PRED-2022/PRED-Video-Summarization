4d3ffecfddbb7d697c225910a792b23cc26ed001
Nathan Rocher <3007nat@gmail.com>
2023-02-10 22:03:38 +0100

    Up nn models

--- graphique_entrainement_v2.py
+++ graphique_entrainement_v2.py
@@ -4,11 +4,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from glob import glob
 
-nn_20 = glob("models/sequence_lstm_nn_512_20_epoch=*.pt")
-nn_75 = glob("models/sequence_lstm_nn_512_75_epoch=*.pt")
-nn_100 = glob("models/sequence_lstm_nn_512_100_epoch=*.pt")
+MODEL_NAME = "sequence_nn_1024_75_epoch=*.pt"
+MODEL_NAME = "sequence_lstm_nn_1024_75_epoch=*.pt"
 
-models = [(nn_20, 20), (nn_75, 75), (nn_100, 100)]
+nn = glob(MODEL_NAME)
+
+models = [(nn, 75)]
 
 values_dict = dict()
 
@@ -18,46 +19,66 @@ for data, nn_size in models:
     values = []
 
     for fn in data:
-        epoch, train_loss, train_r2, val_r2 = [eleme.split("_")[0] for eleme in fn.split("=")][1:]
+        epoch, train_loss, train_r2, val_loss, val_r2 = [eleme.split("_")[0] for eleme in fn.split("=")][1:]
         val_r2 = val_r2.split(".pt")[0]
-        epoch, train_loss, train_r2, val_r2 = int(epoch), float(train_loss), float(train_r2), float(val_r2)
-        values.append((epoch, train_loss, train_r2, val_r2))
+        epoch, train_loss, train_r2, val_loss, val_r2 = int(epoch), float(train_loss), float(train_r2), float(val_loss), float(val_r2)
+        values.append((epoch, train_loss, train_r2, val_loss, val_r2))
 
     values.sort(key=lambda x: x[0])
     values = np.array(values)
     values_dict[nn_size]["epoch"] = values[:, 0]
     values_dict[nn_size]["train_loss"] = values[:, 1]
     values_dict[nn_size]["train_r2"] = values[:, 2]
-    values_dict[nn_size]["val_r2"] = values[:, 3]
+    values_dict[nn_size]["val_loss"] = values[:, 3]
+    values_dict[nn_size]["val_r2"] = values[:, 4]
 
 # sns.set_theme(style="whitegrid")
 
 
+fig, axs = plt.subplots(2, 2)
 
 data = []
 for _, nn_size in models:
     data.append(values_dict[nn_size]["train_loss"])
 data = pd.DataFrame(np.array(data).T, columns=list(map(lambda x: "Séquence : " + str(x[1]), models)))
-ax = sns.lineplot(data=data, dashes=False, linewidth=1.5)
-ax.set(xlabel='Epoch', ylabel='Mean Squared Error sur jeu d\'entraînement')
-plt.show()
+sns.lineplot(ax=axs[0, 0], data=data, dashes=False, linewidth=1.5)
+axs[0, 0].set(xlabel='Epoch', ylabel='Mean Squared Error sur jeu d\'entraînement')
+
+data = []
+for _, nn_size in models:
+    data.append(values_dict[nn_size]["val_loss"])
+data = pd.DataFrame(np.array(data).T, columns=list(map(lambda x: "Séquence : " + str(x[1]), models)))
+sns.lineplot(ax=axs[0, 1], data=data, dashes=False, linewidth=1.5)
+axs[0, 1].set(xlabel='Epoch', ylabel='Mean Squared Error sur jeu de validation')
 
 
 data = []
 for _, nn_size in models:
     data.append(values_dict[nn_size]["train_r2"])
 data = pd.DataFrame(np.array(data).T, columns=list(map(lambda x: "Séquence : " + str(x[1]), models)))
-ax = sns.lineplot(data=data, dashes=False, linewidth=1.5)
-ax.set(xlabel='Epoch', ylabel='R² sur jeu d\'entraînement')
-plt.show()
+sns.lineplot(ax=axs[1, 0], data=data, dashes=False, linewidth=1.5)
+axs[1, 0].set(xlabel='Epoch', ylabel='R² sur jeu d\'entraînement')
+
 
 data = []
 for _, nn_size in models:
     data.append(values_dict[nn_size]["val_r2"])
 data = pd.DataFrame(np.array(data).T, columns=list(map(lambda x: "Séquence : " + str(x[1]), models)))
-ax = sns.lineplot(data=data, dashes=False, linewidth=1.5)
-ax.set(xlabel='Epoch', ylabel='R² sur jeu de validation')
+sns.lineplot(ax=axs[1, 1], data=data, dashes=False, linewidth=1.5)
+axs[1, 1].set(xlabel='Epoch', ylabel='R² sur jeu de validation')
+
+fig.suptitle(MODEL_NAME, fontsize=16)
+
 plt.show()
+
+
+
+
+
+
+
+
+exit()
 
 y_val = []
 x_val = []

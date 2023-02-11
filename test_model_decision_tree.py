25dad29c17722502a389229615329ba16da42d3d
Josik SALLAUD <josik.sallaud@etu.univ-nantes.fr>
2023-02-11 11:37:53 +0100

    Correction du jeu de test

--- test_model_decision_tree.py
+++ test_model_decision_tree.py
@@ -1,10 +1,13 @@
-from math import ceil
+from math import ceil, floor
 import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
+from random import shuffle
+
+TEST_VIDEO = ["i3wAGJaaktw.mp4", "98MoyGZKHXc.mp4", "byxOvuiIJV0.mp4", "eQu1rNs0an0.mp4", "Yi4Ij2NM7U4.mp4", "sTEELN-vY30.mp4", "_xMr-HKMfVA.mp4", "WxtbjNsCQ8A.mp4", "gzDbaEs1Rlg.mp4", "WG0MBPpPC6I.mp4"]
 
 # Ground Truth
 groundtruth_videos = pd.read_csv('./TVSum-groundtruth.csv', sep=';', header=0).set_index('id')
@@ -19,9 +22,8 @@ emotion_videos = pd.read_json("./PROCESSED-TVsum-face-intensity.json", lines=Tru
 memorability_videos = pd.read_csv('./TVSum-memorability.csv', sep=';', header=0).set_index("video_name")
 
 # Dataframe 
-big_df = pd.DataFrame()
+train_df = pd.DataFrame()
 test_df = pd.DataFrame()
-vid_df = pd.DataFrame()
 
 # Création du big tableau
 for key in iovc_videos.keys():
@@ -55,39 +57,76 @@ for key in iovc_videos.keys():
     selected_frames = score_gt[score_gt >= threshold]
     selected_indices = list(selected_frames.index.values)
     df["gt"] = np.where(score_gt >= threshold, 1, 0)
-    big_df = pd.concat([big_df, df], axis= 0)
+    if key in TEST_VIDEO:
+        test_df = pd.concat([test_df, df], axis=0)
+    else:
+        train_df = pd.concat([train_df, df], axis=0)
 
 
-big_gt = big_df["gt"]
-big_df.drop(inplace=True, columns="gt")
-feature_names = big_df.columns
-big_df = big_df.to_numpy()
+train_df.set_index(pd.RangeIndex(len(train_df)), inplace=True)
+test_df.set_index(pd.RangeIndex(len(test_df)), inplace=True)
 
+"""
+
+    Data imbalance :
+    les classes frame selectionnée et frame non sélectionnée sont déséquilibrées.
+    Il y a environ 10-15% de sélectionnées parmi toutes les frames
+
+    On garde autant de frames non selectionnes
+"""
+
+nb_selected_frames = len(train_df[train_df["gt"] == 1])
+nb_not_selected_frames = len(train_df[train_df["gt"] == 0])
+NB_TRAIN_SPLITS = floor(nb_not_selected_frames/nb_selected_frames)
+selected_indices = list(train_df[train_df["gt"] == 1].index.values)
+print("Nb de splits de train :", NB_TRAIN_SPLITS)
+not_selected_indices = list(train_df[train_df["gt"] == 0].index.values)
+shuffle(not_selected_indices)
+
+train_gt = train_df["gt"]
+train_df.drop(inplace=True, columns="gt")
+feature_names = train_df.columns
+
+nb_selected_frames_test = len(test_df[test_df["gt"] == 1])
+nb_not_selected_frames_test = len(test_df[test_df["gt"] == 0])
+NB_TEST_SPLITS = floor(nb_not_selected_frames/nb_selected_frames)
+selected_indices_test = list(test_df[test_df["gt"] == 1].index.values)
+print("Nb de splits de test :", NB_TEST_SPLITS)
+not_selected_indices_test = list(test_df[test_df["gt"] == 0].index.values)
+shuffle(not_selected_indices_test)
+
+test_gt = test_df["gt"]
+test_df.drop(inplace=True, columns="gt")
+
+test_indices = not_selected_indices_test[:nb_selected_frames_test] + selected_indices_test
+print(len(not_selected_indices_test[:nb_selected_frames_test]), len(selected_indices_test))
+test_df = test_df.loc[test_indices]
+test_gt = test_gt.loc[test_indices]
 auc = []
 fp_tp_rates = []
-kf = KFold(n_splits=5, shuffle=True)
 confusion_matrices = []
-for i, (train_index, test_index) in enumerate(kf.split(big_df)):
-    # Récupération des éléments du groupe actuel
-    X_train, X_test = big_df[train_index], big_df[test_index]
-
-    y_train, y_test = big_gt.iloc[train_index], big_gt.iloc[test_index]
+for train_i in range(NB_TRAIN_SPLITS):
+    # Récupération des éléments du groupe actuel d'entrainement
+    first_indice = train_i*nb_selected_frames
+    last_indice = first_indice + nb_selected_frames
+    train_indices = not_selected_indices[first_indice:last_indice] + selected_indices
+    # Jeu d'entrainement
+    X_train, y_train = train_df.loc[train_indices], train_gt.loc[train_indices]
 
     # Entrainement de l'arbre
     binary_classif_model = DecisionTreeClassifier(max_depth=10, random_state=0)
     binary_classif_model.fit(X_train, y_train)
-
     # Prédiction sur les donnees de test
-    y_test_pred = binary_classif_model.predict(X_test)
-    
+    y_test_pred = binary_classif_model.predict(test_df)
+
     # Calcul de la matrice de confusion
-    confusion_matrices.append(metrics.confusion_matrix(y_test, y_test_pred, normalize='pred'))
+    confusion_matrices.append(metrics.confusion_matrix(test_gt, y_test_pred))
 
     # Prédiction des scores plutôt que binaire
-    y_test_pred_score = binary_classif_model.predict_proba(X_test)[:, 1]
+    y_test_pred_score = binary_classif_model.predict_proba(test_df)[:, 1]
 
     # Courbe ROC
-    fp_rate, tp_rate, thresholds = metrics.roc_curve(y_test, y_test_pred_score)
+    fp_rate, tp_rate, thresholds = metrics.roc_curve(test_gt, y_test_pred_score)
     fp_tp_rates.append((fp_rate, tp_rate))
 
     # AUC
@@ -99,7 +138,7 @@ fig, ax = plt.subplots(1, len(confusion_matrices), figsize=(15, 5), sharey='row'
 for i, conf_mat in enumerate(confusion_matrices):
     disp = metrics.ConfusionMatrixDisplay(conf_mat)
     disp.plot(ax=ax[i], xticks_rotation=45)
-    disp.ax_.set_title("Split " + str(i))
+    disp.ax_.set_title("Split " + str(i+1))
     disp.im_.colorbar.remove()
     if i!=0:
         disp.ax_.set_ylabel('')
